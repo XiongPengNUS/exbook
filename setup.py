b54bd22773acb67a25e1273a54474d2c4f7672a5
Peng Xiong <41470721+XiongPengNUS@users.noreply.github.com>
2020-02-07 11:38:53 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='xpy',
+     name='exbook',
      version='0.0.1',
      author="Peng Xiong",
      author_email="xiongpengnus@gmail.com",
      description="Exercises for Python coding",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     url="https://github.com/XiongPengNUS/xpy",
+     url="https://github.com/XiongPengNUS/exbook",
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
